1f2f55fb4f7e5a409c36ea1dded81501c2da09f1
Alan Lee <alan@alans-mbp.lan>
2018-02-27 23:09:50 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+
+function seude(env = '') {
+    if (env.length < 1 || env == null) {
+        throw new Error('A environment string needs to be set')
+    }
+
+    let loaded = {}
+
+    const seudePath = path.resolve(process.cwd(), 'seude.env.json')
+
+    try {
+        const vars = JSON.parse(fs.readFileSync(seudePath))
+        
+        process.env.NODE_ENV = env
+        
+        if (vars[env]) {
+            for (let [x, y] of Object.entries(vars[env])) {
+                process.env[x] = y
+                loaded[x] = y
+            }
+        }
+    } catch(e) {
+        throw new Error(e)
+    }
+
+    return { ...loaded }
+}
+
+module.exports = seude
